1bd88683e192772a61819648727ae06b2057c81b
GyeongSu Han <micalgenus@gmail.com>
2020-02-04 23:00:49 +0900

    Initialize express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-export default true;
+import express from 'express';
+import createError from 'http-errors';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+app.get('/webhook', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('PR Bot listening on port 3000!');
+});
