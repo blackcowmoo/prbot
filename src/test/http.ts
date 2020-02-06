8b98f0771f7ffcd50ed51fb22065fdbc748c0f36
GyeongSu Han <micalgenus@gmail.com>
2020-02-07 00:02:36 +0900

    Fix lint syntax

--- src/test/http.ts
+++ src/test/http.ts
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
+import { Response } from 'superagent';
 import express from 'express';
 import { webhookHandler } from '@/core/handler';
 
@@ -9,10 +10,10 @@ chai.should();
 
 export const expect = chai.expect;
 
-type Response = any;
+// type Response = any;
 type methods = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
-interface chaiRequest {
+interface ChaiRequest {
   params?: any;
   body?: any;
   authorization?: string;
@@ -21,17 +22,26 @@ interface chaiRequest {
 const app = express();
 app.get('/webhook', webhookHandler);
 
-export const requestAsync = async <T = Response>(method: methods, path: string, { authorization, body, params }: chaiRequest = {}) => {
+export const requestAsync = async <T extends Response = any>(method: methods, path: string, request: ChaiRequest = {}) => {
+  switch (method.toLowerCase()) {
+    case 'get':
+      return requestGetAsync<T>(path, request);
+    default:
+      throw new Error('Invalid method');
+  }
+};
+
+export const requestGetAsync = async <T extends Response = any>(path: string, { authorization, body, params }: ChaiRequest = {}) => {
   return new Promise<T>((resolve, reject) => {
     return chai
       .request(app)
-      [method.toLowerCase()](path)
+      .get(path)
       .query(params || {})
       .set('authorization', authorization || '')
       .send(body || {})
       .end((err, res) => {
         if (err) return reject(err);
-        return resolve(res);
+        return resolve(res as T);
       });
   });
 };
