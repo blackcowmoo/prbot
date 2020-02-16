4c29d061a17bf4c6710c20ad5b524d092c4a4ca2
GyeongSu Han <micalgenus@gmail.com>
2020-02-16 20:40:28 +0900

    Add github apis

--- src/core/github.ts
+++ src/core/github.ts
@@ -1,15 +1,30 @@
-import GitHub from 'github-api';
-// const gh = new GitHub();
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 class GithubAPI {
-  private client: GitHub;
-
+  private client: AxiosInstance;
   constructor(username?: string, password?: string) {
+    const axiosConfig: AxiosRequestConfig = {
+      baseURL: 'https://api.github.com',
+      headers: {
+        Accept: 'application/vnd.github.inertia-preview+json',
+      },
+    };
+
     if (username && password) {
-      this.client = new GitHub({ username, password });
-    } else {
-      this.client = new GitHub();
+      axiosConfig.auth = { username, password };
     }
+
+    this.client = axios.create(axiosConfig);
+  }
+
+  public async getAllOrganizationRepositories(org: string) {
+    const { data } = await this.client.get(`/orgs/${org}/repos`);
+    return data;
+  }
+
+  public async getCollaborators(org: string, project: string) {
+    const { data } = await this.client.get(`/repos/${org}/${project}/contributors`).catch(err => err);
+    return data;
   }
 }
 
