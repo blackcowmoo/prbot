410b390784a1cc3d7d1bc3307f8d49e35ecf765d
GyeongSu Han <micalgenus@gmail.com>
2020-02-16 22:37:17 +0900

    리뷰어 등록해주는 웹훅 기능 구현

--- src/core/handler.ts
+++ src/core/handler.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from 'express';
 import { convertKeyToLowerCase } from '@/core/util';
+import github from './github';
 
 export const webhookHandler = async (req: Request, res: Response) => {
   const headers = convertKeyToLowerCase(req.headers);
 
   switch (headers['x-github-event']) {
     case 'pull_request':
+      const { number, state, requested_reviewers, user } = req.body.pull_request;
+      if (state === 'open' && requested_reviewers.length === 0) {
+        const ignoreMembers = [user.login];
+        if (process.env.GITHUB_USERNAME) {
+          ignoreMembers.push(process.env.GITHUB_USERNAME);
+        }
+
+        github.requestReview(number, ignoreMembers);
+      }
       break;
     default:
       throw new Error('Invalid event');
